eca5d1f0a9a741a3fde7b7285e415df622136449
jtsang02 <jtsang02@student.ubc.ca>
2023-01-31 01:30:39 -0800

    boostrap navbar

--- Software/frontend/src/App.tsx
+++ Software/frontend/src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css';
 import { BrainChart } from './Components/Charts/BrainChart';
 import { PetNavBar } from './Components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-    
+      <PetNavBar />
       <header className="App-header">
-        <div className="Navbar">
-          <PetNavBar />
-        </div>
         <div className="Chart">
           <BrainChart />
         </div>

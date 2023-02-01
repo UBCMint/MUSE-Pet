f126064d6751686c82da91192f83ae2a89159b2b
jtsang02 <jtsang02@student.ubc.ca>
2023-01-31 23:07:04 -0800

    navbar logos

--- Software/frontend/src/Components/NavBar/NavBar.tsx
+++ Software/frontend/src/Components/NavBar/NavBar.tsx
@@ -3,13 +3,14 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const PetNavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="dark" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Brand href="#">Hello Mint 👋
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -17,22 +18,9 @@ export const PetNavBar = () => {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
-                        </Nav.Link>
+                        <Nav.Link href="#userProfile">Profile</Nav.Link>
                     </Nav>
+
                     <Form className="d-flex">
                         <Form.Control
                             type="search"
@@ -41,7 +29,16 @@ export const PetNavBar = () => {
                             aria-label="Search"
                         />
                         <Button variant="outline-success">Search</Button>
+                        <img
+                            src='/images/mintLogoTransparent.png'
+                            alt="Mint Logo"
+                            width="35"
+                            height="35"
+                            className='d-inline-block align-middle'
+                            style={{ marginLeft: '10px'}}
+                        />
                     </Form>
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>

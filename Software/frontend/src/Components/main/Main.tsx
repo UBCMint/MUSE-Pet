d4c014731b5e3c80d0c90490b680a152654d69b4
Peter Na <ruikangna0605@hotmail.com>
2023-02-11 14:40:24 -0800

    add main component

---
+++ Software/frontend/src/Components/main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Main: React.FC<{}> = () => {
+    return (
+    <div className="main">
+        <h1>MINT Tamagochis</h1>
+        <button>Start</button>
+    </div>
+    )
+}
+
+export default Main
+

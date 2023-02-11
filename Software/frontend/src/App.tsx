4ec5d1c9c88ebf73c2d0839f0ae78b4653aee27e
Peter Na <ruikangna0605@hotmail.com>
2023-02-11 14:39:58 -0800

    add routes to app

--- Software/frontend/src/App.tsx
+++ Software/frontend/src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { BrainChart } from './Components/Charts/BrainChart';
+import Main from './Components/main/Main'
 
 function App() {
   return (
-    <div className="App">
-    
-      <header className="App-header">
-        <div className="Chart">
-          <BrainChart />
-        </div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={< Main />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

60542da0ee6c1fa35b0880b6ad9c2380bd9e5bc3
Peter Na <ruikangna0605@hotmail.com>
2023-02-12 02:42:51 -0800

    add styles and update add pet component

--- Software/frontend/src/Components/addPets/AddPets.tsx
+++ Software/frontend/src/Components/addPets/AddPets.tsx
@@ -1,24 +1,49 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import PetModel from '../../Models/PetModel'
 import './AddPets.css'
+import PetListModel from '../../Models/PetListModel'
 
 const AddPets: React.FC<{}> = () => {
+    const [name, setName]: [string, (name: string) => void] = useState("")
+    const [tirednessLevel, setTirednessLevel]: [number, (tirednessLevel: number) => void] = useState(0)
+
+    const handleSubmit = () => {
+        return async (event: React.MouseEvent) => {
+            event.preventDefault()  // prevent submitting default/empty form
+            const request = await axios.post<PetListModel>('http://localhost:9000/pet', {name: name, focusLevel: tirednessLevel})
+        }
+    }
+
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const tempName = event.target.value;
+        setName(tempName);
+    }
+
+    const handleTirednessLevelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const tempTirednessLevel = parseInt(event.target.value);
+        setTirednessLevel(tempTirednessLevel);
+    }
+
     return (
-    <div className="addPets">
-        <div className='createForm'>
-            <h1>Create a New Pet</h1>
-            <h3>Name</h3>
-            <input></input>
-            <h3>Tiredness Level</h3>
-            <input></input>
-            <div className='formButtons'>
-                <Link to='/pets'>
-                    <button id='cancelButton'>Get Started</button>
-                </Link>
-                <button id='createButton'>Create</button>
+        <div className="addPets">
+            <div className='form'>
+                <h1>Create a New Pet</h1>
+                <div className='formContent'>
+                    <h3>Name</h3>
+                    <input onChange={handleNameChange}></input>
+                    <h3>Tiredness Level</h3>
+                    <input onChange={handleTirednessLevelChange}></input>
+                </div>
+                <div className='formButtons'>
+                    <Link to='/pets'>
+                        <button id='cancelButton'>Back</button>
+                    </Link>
+                    <button id='createButton' onClick={handleSubmit}>Create</button>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 

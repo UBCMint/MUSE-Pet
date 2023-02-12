3ad1ba5391a2643119b624e51703298818eae096
jtsang02 <jtsang02@student.ubc.ca>
2023-02-12 03:41:56 -0800

    navbar and user profile page
    user profile page just a template for now

--- Software/frontend/src/Components/NavBar/NavBar.tsx
+++ Software/frontend/src/Components/NavBar/NavBar.tsx
@@ -3,44 +3,57 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Profile } from '../Profile/Profile';
+import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export const PetNavBar = () => {
     return (
-        <Navbar bg="dark" expand="lg">
-            <Container fluid>
-                <Navbar.Brand href="#">Hello Mint 👋
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#userProfile">Profile</Nav.Link>
-                    </Nav>
+        <Router>
+            <Navbar bg="dark" expand="lg">
+                <Container fluid>
+                    <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+                    <Navbar.Brand
+                        style={{ color: 'white' }}
+                    >Hello Mint{
+                            // name of user goes here, to be passed in as a prop
+                        } 👋
+                    </Navbar.Brand>
+                    </Link>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Link to="/profile" style={{
+                                color: 'white', textDecoration: 'none'
+                            }}>Profile</Link>
+                        </Nav>
+                        <Form className="d-flex">
+                            <Form.Control
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                            <img
+                                src='/images/mintLogoTransparent.png'
+                                alt="Mint Logo"
+                                width="35"
+                                height="35"
+                                className='d-inline-block align-middle'
+                                style={{ marginLeft: '10px' }}
+                            />
+                        </Form>
 
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                        <img
-                            src='/images/mintLogoTransparent.png'
-                            alt="Mint Logo"
-                            width="35"
-                            height="35"
-                            className='d-inline-block align-middle'
-                            style={{ marginLeft: '10px'}}
-                        />
-                    </Form>
-
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Routes>
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </Router>
     );
 };

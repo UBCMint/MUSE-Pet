ee890ed8f0757ebf9f18cd93041445966e2d8aa6
Peter Na <ruikangna0605@hotmail.com>
2023-02-11 18:35:11 -0800

    use props in pet.tsx

--- Software/frontend/src/Components/pets/pet/Pet.tsx
+++ Software/frontend/src/Components/pets/pet/Pet.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Pet.css'
+import PetListModel from '../../../Models/PetListModel'
 
-const Pet: React.FC<{}> = () => {
+const Pet: React.FC<PetListModel> = (props) => {
     return (
     <div className="pet">
         <Link to='/pet' style={{ textDecoration: 'none' }}>
-            <h2>Tamagochi</h2>
+            <h2>{props.name}</h2>
         </Link>
-        <h5>Focus Level: {50}</h5>
-        <progress value="50" max="100">10</progress>
+        <h5>Focus Level: {props.focusLevel}</h5>
+        <progress value={props.focusLevel} max="100">10</progress>
     </div>
     )
 }

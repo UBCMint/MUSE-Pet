24c0bb2137f5f7fd610807b39bc1f3066ef698a2
Peter Na <ruikangna0605@hotmail.com>
2023-02-20 21:03:30 -0800

    Add pet details page

---
+++ Software/frontend/src/Components/pets/petDetails/PetDetails.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import PetAnimations from '../petAnimations/petAnimations'
+import './PetDetails.css'
+
+const PetDetails: React.FC<{}> = () => {
+    return (
+        <PetAnimations />
+    )
+}
+
+export default PetDetails

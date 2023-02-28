92eb5fea3a5585f0b4e1312aefa143d2c6693bd3
Peter Na <ruikangna0605@hotmail.com>
2023-02-27 23:28:53 -0800

    remove mongoose braindata schema

--- Software/backend/routes/brainData.js
+++ Software/backend/routes/brainData.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const BrainData = require('../models/brainData')
 
 router.get('/', async(req,res) => {
 })
 
 router.post('/', async(req,res) => {
-    const brainData = new BrainData({
+    const brainData = new {
         focusLevel: req.body.focusLevel,
         brainWaves: req.body.brainWaves
-    })
+    }
 })

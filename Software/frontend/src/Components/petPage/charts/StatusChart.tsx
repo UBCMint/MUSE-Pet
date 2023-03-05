044383bda5279bf48ea28cd16582c963b1cb5f4b
Peter Na <ruikangna0605@hotmail.com>
2023-03-04 23:44:52 -0800

    working status chart

--- Software/frontend/src/Components/petPage/charts/StatusChart.tsx
+++ Software/frontend/src/Components/petPage/charts/StatusChart.tsx
@@ -1,23 +1,48 @@
 import { Bar } from 'react-chartjs-2'
+import { useState } from 'react';
+import BrainData from '../../../Models/BrainData'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-export const StatusChart = () => {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const StatusChart: React.FC<BrainData> = (props: BrainData) => {
+  const petAttibutes = [props.focusLevel, props.tirednessLevel, props.happinessLevel]
+
   return (
-    <div>
-      <Bar 
+    <Bar 
         data={
           {
-            labels: ['Health', 'Hunger', 'Happiness'],
+            labels: ['Focus Level', 'Tiredness Level', 'Happiness Level'],
             datasets: [{
               label: 'Pet Statuses',
-              data: [12, 12, 12]
+              data: petAttibutes,
+              backgroundColor: [
+                '#32cec6'
+              ],
+              borderColor: [
+                '#32cec6'
+              ],
+              borderWidth: 1
             }]
           }
         }
-        height={400}
-        width={600}
+        height={250}
+        width={400}
       />
-    </div>
   )
 }
-
-export default StatusChart

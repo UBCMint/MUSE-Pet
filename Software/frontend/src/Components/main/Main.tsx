f01f0b93698ae88f3436ac007e184e7041d9d917
jtsang02 <jtsang02@student.ubc.ca>
2023-03-09 02:09:05 -0800

    more styling and background

--- Software/frontend/src/Components/main/Main.tsx
+++ Software/frontend/src/Components/main/Main.tsx
@@ -36,31 +36,31 @@ const Main: React.FC<{}> = () => {
                     <Container className="features-icons text-center">
                         <Row className='Row'>
                             <div className="col-lg-4">
-                                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 text-white">
                                     <div className="features-icons-icon d-flex"><i className="bi-window m-auto text-danger">
                                         <FaBrain />
                                     </i></div>
                                     <h3>Brain Signals</h3>
-                                    <p className="lead mb-0">Get instant feedback from your Pet's brain.</p>
+                                    <p className="lead mb-0 text-dark">Get instant feedback from your Pet's brain.</p>
                                 </div>
                             </div>
                             <div className="col-lg-4">
-                                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                                <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3 text-white">
                                     <div className="features-icons-icon d-flex"><i className="bi-layers m-auto text-primary">
                                         <FcHeadset />
                                     </i></div>
                                     <h3>EEG Compatible</h3>
-                                    <p className="lead mb-0">Featuring Compatible with the latest EEG technology.</p>
+                                    <p className="lead mb-0 text-dark">Featuring Compatible with the latest EEG technology.</p>
                                 </div>
                             </div>
                             <div className="col-lg-4">
-                                <div className="features-icons-item mx-auto mb-0 mb-lg-3">
+                                <div className="features-icons-item mx-auto mb-0 mb-lg-3 text-white">
                                     <div className="features-icons-icon d-flex">
                                         <i className="bi-terminal m-auto text-success">
                                             <GoGraph />
                                         </i></div>
                                     <h3>Analyze Progress</h3>
-                                    <p className="lead mb-0">Track Alpha, Beta and Gamma Signals.</p>
+                                    <p className="lead mb-0 text-dark">Track Alpha, Beta and Gamma Signals.</p>
                                 </div>
                             </div>
                         </Row>

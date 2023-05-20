fab08c692b7b5fed0d8f7454da08303e298ad143
jtsang02 <jtsang02@student.ubc.ca>
2023-05-20 03:20:37 -0700

    searchbar
    implemented for fun

--- Software/frontend/src/Components/NavBar/NavBar.tsx
+++ Software/frontend/src/Components/NavBar/NavBar.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
@@ -24,7 +22,7 @@ export const PetNavBar: React.FC<{}> = () => {
           </Link>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+            <Nav className="m-auto my-2 my-lg-0" navbarScroll>
               <Nav.Link as={Link} to="/pets" style={{ color: "grey" }}>
                 My Pets
               </Nav.Link>
@@ -35,14 +33,6 @@ export const PetNavBar: React.FC<{}> = () => {
                 Sign Out
               </Nav.Link>
             </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
               <img
                 src="/images/mintLogoTransparent.png"
                 alt="Mint Logo"
@@ -51,7 +41,6 @@ export const PetNavBar: React.FC<{}> = () => {
                 className="d-inline-block align-middle"
                 style={{ marginLeft: "10px" }}
               />
-            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
--- Software/frontend/src/Components/pets/Pets.tsx
+++ Software/frontend/src/Components/pets/Pets.tsx
@@ -6,11 +6,11 @@ import "./Pets.css";
 import PetModel from "../../Models/PetModel";
 import PetNavBar from "../navBar/NavBar";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Pets: React.FC<{}> = () => {
   const defaultPets: PetModel[] = [];
-  const [pets, setPets]: [PetModel[], (pets: PetModel[]) => void] =
-    useState(defaultPets);
+  const [pets, setPets]: [PetModel[], (pets: PetModel[]) => void] = useState(defaultPets);
 
   const getPets = async () => {
     const response = await axios.get<PetModel[]>("http://localhost:9000/pet");
@@ -30,18 +30,41 @@ const Pets: React.FC<{}> = () => {
     getPets();
   }, []);
 
+
+  const filterPets = async (value: string) => {
+    const response = await axios.get("http://localhost:9000/pet").then((res) => {
+      // filter the response to only include pets that match the search term
+      const filtered = res.data.filter((pet: any) => {
+        return pet.name.toLowerCase().includes(value.toLowerCase());
+      });
+      return filtered;
+    });
+    setPets(response);
+  };
+
+  const handleChange = (value: string) => {
+    filterPets(value);
+  };
+
   return (
     <div>
       <PetNavBar />
       <div className="pets">
-        <div className="header">
+        <div className="header" style={{marginBottom: "1em"}}>
           <h1>Tamagochis</h1>
         </div>
-        <Link to="/addPets" style={{ textDecoration: "none" }}>
-          <Button variant="info" className="create-btn">
-            Create Pet
-          </Button>
-        </Link>
+
+        <Form className="d-flex" style={
+          { width: "50%", maxWidth: "550px", display: "flex", justifyContent: "center", alignItems: "center", margin: "auto" }
+        }>
+          <Form.Control
+            type="search"
+            placeholder="Search pet name..."
+            aria-label="Search"
+            id="searchInput"
+            onChange={(e) => handleChange(e.target.value)}
+          />
+        </Form>
         {pets.map((pet) => (
           <Pet
             key={pet._id}
@@ -56,6 +79,11 @@ const Pets: React.FC<{}> = () => {
             handleDelete={handleDelete}
           />
         ))}
+        <Link to="/addPets" style={{ textDecoration: "none" }}>
+          <Button variant="info" className="create-btn">
+            Create Pet
+          </Button>
+        </Link>
       </div>
     </div>
   );
